2fcc98bf12a13b29d4cb191ec5464f6547a72b8f
RebecaVega <95606364+RebecaVega@users.noreply.github.com>
2022-06-02 15:55:05 -0500

    Add files via upload

---
+++ vega_escamillaEP3_2.R
@@ -0,0 +1,15 @@
+### REBECA VEGA ESCAMILLA ###
+
+#2.A partir de un objeto de tipo phyloseq generado de un análisis  de 
+#identificación taxonómica  a partir del gen 16S ribosomal elabora un programa 
+#que
+
+#primero cargo mis librerias
+library(phyloseq)
+library(ggplot2)
+
+#1. Calcule distintas medidas de diversidad
+#2. Elabore una gráfica de barras de abundancias por muestras.
+#3. Elabore un análisis de reducción de dimensionalidad
+#4.  Muestre el microbioma core de las muestras
+#5. (Opcional) genere redes de co-abundacia por muestra.
